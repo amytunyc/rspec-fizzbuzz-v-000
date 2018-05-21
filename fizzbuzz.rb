bde7d70508e3342cd9e22be7b363fbc86d326b56
Amy Tu <siamesebb@gmail.com>
2018-05-21 22:36:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  elseif int % 5 == 0
+  end
+  if int % 5 == 0
     puts "Buzz"
-  elseif int % 3 == 0 && int % 5 == 0
+  end
+  if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
-    end
+  end
+end
   
